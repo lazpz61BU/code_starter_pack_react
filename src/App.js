c3d01628fc3023f9c7f7d3969cc748f50ba62265
tiyu0203 <tiyu0203@gmail.com>
2022-01-27 23:42:04 -0500

    fetches data from database and displays as a tablw for now

--- src/App.js
+++ src/App.js
@@ -1,31 +1,93 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { Component } from "react";
+// import {Button, Row, Col, CardImg, CardTitle, CardSubtitle, CardText, CardBody,
+//   FormFeedback, Container, Card, Modal, ModalHeader, ModalBody, Input, FormText
+// } from 'reactstrap';
+// https://learn.co/lessons/react-container-components
+//https://blog.logrocket.com/react-native-sectionlist-tutorial-examples/
+class App extends React.Component {
+   
+    // Constructor 
+    constructor(props) {
+        super(props);
+   
+        this.state = {
+            items: [],
+            DataisLoaded: false            
+        };
+    }
+   
+    // ComponentDidMount is used to
+    // execute the code 
+    //fetches the data as a json from the database
+    componentDidMount() {
+        fetch(
+"https://code-starter-pack-python.herokuapp.com/website/get")
+            .then((res) => res.json())
+            .then((json) => {
+                this.setState({
+                    items: json,
+                    DataisLoaded: true
+                });
+            })
+    }
+    //redners the data in a table
+    renderTableData() {
+      return this.state.items.map((website, index) => {
+         const { id, category, resource, url } = website //destructuring
+         return (
+            <tr key={id}>
+               <td>{id}</td>
+               <td>{category}</td>
+               <td>{resource}</td>
+               <td>{url}</td>
+            </tr>
+         )
+      })
+   }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <div>
-          This is for to test a chan
+    render() {
+        const { DataisLoaded, items } = this.state;
+        if (!DataisLoaded) return <div>
+            <h1> Pleses wait some time.... </h1> </div> ;
+        //getting all the categories
+        const categories = new Set()
+        for(var website in items) {
+          for (var detail in items[website]) {
+            if (detail === "category"){
+              categories.add(items[website][detail])
+            }
+          }
+        }
+      
+        return (
+        <div className = "App">
+          <header className="App-header">
+            <div>
+              <h1 id='title'>Code Starter Pack</h1>
+              <table id='websites'>
+                <tbody>
+                    {this.renderTableData()}
+                </tbody>
+              </table>
+            </div>
+            {/* <div>
+              <h1> Fetch data from an api in react </h1>  {
+                  items.map((item) => ( 
+                  <ol key = { item.id } >
+                      category: { item.category }, 
+                      resource: { item.resource }, 
+                      url: { item.url } 
+                      </ol>
+                  ))
+              }
+            </div> */}
+          </header>
+
         </div>
-        <div>
-          Another Test
-        </div>
-      </header>
-    </div>
-  );
+    );
 }
-
+}
+   
 export default App;
