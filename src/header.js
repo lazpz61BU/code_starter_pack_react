c524ac0d3da3584f16348cfb833c35eeb97b208a
Lazaro Perez <lazpz61@bu.edu>
2022-02-14 02:01:02 -0500

    adding back useEffect for filter rendering

--- src/header.js
+++ src/header.js
@@ -16,6 +16,10 @@ export default function Header(props) {
 
   const [selected, setSelected] = useState('Filter by Category');
 
+  useEffect(() => {
+    props.handleDropdownUpdate(selected);
+  }, [selected]);
+
   const handleSelect = (value) => {
     props.handleDropdownUpdate(value);
     setSelected(value);
@@ -37,7 +41,7 @@ export default function Header(props) {
       </h1>
 
       <DropdownButton id="dropdown-basic-button" title={selected} onSelect={handleSelect}>
-        <Dropdown.Item key="Filter by Category" eventKey="Filter by Category">Clear Dropdown</Dropdown.Item>
+        <Dropdown.Item key="Filter by Category" eventKey="Filter by Category">Clear Filter</Dropdown.Item>
         {Object.entries(props.resources).map(([key]) => (<Dropdown.Item key={key} eventKey={key}>{key}</Dropdown.Item>))}
       </DropdownButton>
 
